a75863e3b3e8347e239332974c6d2757101cedff
Angelica <agervais97@gmail.com>
2022-03-02 13:05:57 -0500

    completed exercise

---
+++ arrowfunc.js
@@ -0,0 +1,12 @@
+function double(arr) {
+    return arr.map(function(val){
+        return val *2;
+    })
+}
+
+double(arr) => arr.map(val => val *2);
+
+squareAndFindEvens(numbers) => evens 
+
+let squares = numbers.map(num => num ** 2)
+let evens = sqaures.filter(square => square % 2 === 0)
